67377edf9c1555905fff5f5dc4ff43b12aa941b2
Ridham2001 <201801006@daiict.ac.in>
2021-05-29 10:14:55 +0530

    Add: basic setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import './App.css'
-import Home from './pages/Home'
+import "./App.css";
+import { useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
-	return (
-		<div>
-			<Header />
-			<Home />
-			<Footer />
-		</div>
-	)
+    const dispatch = useDispatch();
+    const auth = useSelector((state) => state.auth);
+
+    return (
+        <div className="App">
+            <Switch>
+                <Header />
+                <Home />
+                <Footer />
+            </Switch>
+        </div>
+    );
 }
 
-export default App
+export default App;

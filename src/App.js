34a4ee1d6367281ddc5567cebfe9961516aaf991
Kushagra Pathak <kushagrapathak@Kushagras-MacBook-Air.local>
2021-05-29 14:54:10 +0530

    Improved Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { BrowserRouter as Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import {Home , Dashboard, Login, SignUp, Market,Mandis,Produce} from './pages'
+import {
+	Home,
+	Dashboard,
+	Login,
+	SignUp,
+	Market,
+	Mandis,
+	Produce,
+} from './pages'
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<Switch>
-				<Route exact path={'/'} component={Home} />
-				<Route exact path={'/registration'} component={SignUp} />
-				<Route exact path={'/login'} component={Login} />
-				<Route exact path={'/dashboard'} component={Dashboard} />
-				<Route exact path={'/market'} component={Market} />
-				<Route exact path={'/mandis'} component={Mandis} />
-				<Route exact path={'/produce'} component={Produce} />
-			</Switch>
+			<Router>
+				<Switch>
+					<Route exact path={'/'} component={Home} />
+					<Route exact path={'/registration'} component={SignUp} />
+					<Route exact path={'/login'} component={Login} />
+					<Route exact path={'/dashboard'} component={Dashboard} />
+					<Route exact path={'/market'} component={Market} />
+					<Route exact path={'/mandis'} component={Mandis} />
+					<Route exact path={'/produce'} component={Produce} />
+				</Switch>
+			</Router>
 			<Footer />
 		</div>
 	)
